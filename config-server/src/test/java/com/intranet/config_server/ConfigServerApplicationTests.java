08942dfdae09d3274a70f65d104c6b72d6d703b9
NOEL318 <noelrinaya318@gmail.com>
2025-06-23 21:33:49 -0600

    API-Gateway added, but not working yet.

---
+++ config-server/src/test/java/com/intranet/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intranet.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
